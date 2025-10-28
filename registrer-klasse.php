e320521945ff09ad076cc6b798bb5071bba0ed12
Haahm608 <hamseilyas@hotmail.com>
2025-10-28 16:54:14 +0100

    nn

--- registrer-klasse.php
+++ registrer-klasse.php
@@ -1,36 +1,73 @@
 <?php
+
 include("db-tilkobling.php"); // kobler til databasen
  
 if (isset($_POST["registrerKlasseKnapp"])) {
+
     $klassekode  = trim($_POST["klassekode"]);
+
     $klassenavn  = trim($_POST["klassenavn"]);
+
     $studiumkode = trim($_POST["studiumkode"]);
  
+    // Sjekk at alle felt er fylt ut
+
     if (!$klassekode || !$klassenavn || !$studiumkode) {
+
         print("<p style='color:red'>Alle felt må fylles ut.</p>");
-    } else {
-        // sjekk om klassen finnes fra før
+
+    } 
+
+    // Sjekk at klassekode ikke overskrider 10 tegn
+
+    else if (strlen($klassekode) > 10) {
+
+        print("<p style='color:red'>Klassekode kan maks være 10 tegn.</p>");
+
+    } 
+
+    else {
+
+        // Sjekk om klassen finnes fra før
+
         $sqlSjekk = "SELECT * FROM klasse WHERE klassekode = '$klassekode'";
+
         $resultat = mysqli_query($db, $sqlSjekk);
  
         if (mysqli_num_rows($resultat) > 0) {
+
             print("<p style='color:orange'>Klassen finnes allerede.</p>");
+
         } else {
-            // legg til ny klasse
+
+            // Legg til ny klasse
+
             $sqlSettInn = "INSERT INTO klasse (klassekode, klassenavn, studiumkode)
+
                            VALUES ('$klassekode', '$klassenavn', '$studiumkode')";
+
             if (mysqli_query($db, $sqlSettInn)) {
+
                 print("<p style='color:green'>
+
                         Klassen '$klassekode – $klassenavn' ble registrert!
 </p>");
+
             } else {
+
                 print("<p style='color:red'>
+
                         Feil ved registrering: " . mysqli_error($db) . "
 </p>");
+
             }
+
         }
+
     }
+
 }
+
 ?>
  
 <!DOCTYPE html>
@@ -43,8 +80,11 @@ if (isset($_POST["registrerKlasseKnapp"])) {
  
 <h3>Registrer ny klasse</h3>
 <form method="post" action="">
-    Klassekode: <input type="text" name="klassekode" maxlength="5" required><br><br>
+
+    Klassekode: <input type="text" name="klassekode" maxlength="10" required><br><br>
+
     Klassenavn: <input type="text" name="klassenavn" required><br><br>
+
     Studiumkode: <input type="text" name="studiumkode" required><br><br>
 <input type="submit" name="registrerKlasseKnapp" value="Registrer klasse">
 <input type="reset" value="Nullstill">
@@ -54,4 +94,5 @@ if (isset($_POST["registrerKlasseKnapp"])) {
  
 </body>
 </html>
+
  
