af1f82cccd750b95c321014adef08b297b39ceae
Haahm608 <hamseilyas@hotmail.com>
2025-10-28 02:20:40 +0100

    hh

---
+++ registrer-student.php
@@ -0,0 +1,78 @@
+<?php
+include("db-tilkobling.php");
+ 
+if (isset($_POST["registrerStudentKnapp"])) {
+    $brukernavn = trim($_POST["brukernavn"]);
+    $fornavn    = trim($_POST["fornavn"]);
+    $etternavn  = trim($_POST["etternavn"]);
+    $klassekode = trim($_POST["klassekode"]);
+ 
+    // Validering basert på databasestrukturen
+    if (strlen($brukernavn) != 7) {
+        print("<p style='color:red'>Brukernavn må være nøyaktig 7 tegn.</p>");
+    }
+    elseif (strlen($fornavn) > 50) {
+        print("<p style='color:red'>Fornavn kan maks være 50 tegn.</p>");
+    }
+    elseif (strlen($etternavn) > 50) {
+        print("<p style='color:red'>Etternavn kan maks være 50 tegn.</p>");
+    }
+    elseif (!$brukernavn || !$fornavn || !$etternavn || !$klassekode) {
+        print("<p style='color:red'>Alle felt må fylles ut.</p>");
+    } else {
+        // Sjekk om studenten finnes fra før
+        $sqlSjekk = "SELECT * FROM student WHERE brukernavn = '$brukernavn'";
+        $resultat = mysqli_query($db, $sqlSjekk);
+ 
+        if (mysqli_num_rows($resultat) > 0) {
+            print("<p style='color:orange'>Studenten finnes allerede.</p>");
+        } else {
+            // Legg til ny student
+            $sqlSettInn = "INSERT INTO student (brukernavn, fornavn, etternavn, klassekode)
+                           VALUES ('$brukernavn', '$fornavn', '$etternavn', '$klassekode')";
+            if (mysqli_query($db, $sqlSettInn)) {
+                print("<p style='color:green'>
+                        Studenten '$brukernavn - $fornavn $etternavn' ble registrert!
+</p>");
+            } else {
+                print("<p style='color:red'>
+                        Feil ved registrering: " . mysqli_error($db) . "
+</p>");
+            }
+        }
+    }
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="no">
+<head>
+<meta charset="UTF-8">
+<title>Registrer student</title>
+</head>
+<body>
+ 
+<h3>Registrer student</h3>
+<form method="post" action="">
+    Brukernavn: <input type="text" name="brukernavn" maxlength="7" required><br>
+<small>Må være nøyaktig 7 tegn</small><br><br>
+    Fornavn: <input type="text" name="fornavn" maxlength="50" required><br><br>
+    Etternavn: <input type="text" name="etternavn" maxlength="50" required><br><br>
+    Klassekode:
+<select name="klassekode" required>
+<option value="">Velg klasse</option>
+<?php
+        include("dynamiske-funksjoner.php");
+        listeboksKlassekode();
+        ?>
+</select><br><br>
+<input type="submit" name="registrerStudentKnapp" value="Registrer student">
+<input type="reset" value="Nullstill">
+</form>
+ 
+<p><a href="vis-alle-studenter.php">Vis alle studenter</a></p>
+ 
+</body>
+</html>
+ 
+ 
---
+++ slett-klasse.php
@@ -0,0 +1,42 @@
+<?php /* slett-klasse */
+/*
+/* Programmet lager et skjema for å kunne slette en klasse
+/* Programmet sletter den valgte klassen
+*/
+?>
+<script src="funksjoner.js"> </script>
+<h3>Slett klasse</h3>
+<form method="post" action="" id="slettklasseSkjema" name="slettklasseSkjema" onSubmit="return bekreft()">
+Klasse
+<select name="klassekode" id="klassekode">
+<?php print("<option value=''>velg klasse </option>");
+include("dynamiske-funksjoner.php"); listeboksklassekode(); ?>
+</select> <br/>
+<input type="submit" value="Slett klasse" name="slettklasseKnapp" id="slettklasseKnapp" />
+</form>
+<?php
+if (isset($_POST ["slettklasseKnapp"]))
+{
+$klassekode=$_POST ["klassekode"];
+if (!$klassekode)
+{
+print ("Det er ikke valgt noe klasse");
+}
+else
+{
+include("db-tilkobling.php"); /* tilkobling til database-serveren utført og valg av database foretatt */
+ 
+// Her begynner alternativ 1: slett studentene først
+mysqli_query($db, "DELETE FROM student WHERE klassekode='$klassekode'")
+    or die("Ikke mulig å slette studenter fra databasen.");
+ 
+// Slett deretter klassen
+$sqlSetning="DELETE FROM klasse WHERE klassekode='$klassekode';";
+mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; slette data i databasen");
+/* SQL-setning sendt til database-serveren */
+print ("F&oslash;lgende klasse er n&aring; slettet: $klassekode <br />");
+}
+}
+?>
+ 
+ 
