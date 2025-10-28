4a17a1add7ab1088896e0224aa6a79f507190406
Haahm608 <hamseilyas@hotmail.com>
2025-10-28 20:56:54 +0100

    haha

--- dynamiskefunksjoner.php
+++ dynamiskefunksjoner.php
@@ -1,70 +1,21 @@
 <?php
-
-function listeboksKlassekode() {
-
-    include("db-tilkobling.php");
-
-    $sql = "SELECT * FROM klasse ORDER BY klassekode;";
-
-    $resultat = mysqli_query($db,$sql) or die("Feil ved henting av klasser: " . mysqli_error($db));
-
-    while ($rad = mysqli_fetch_array($resultat)) {
-
-        $kode = $rad["klassekode"];
-
-        $navn = $rad["klassenavn"];
-
-        print("<option value='$kode'>$kode - $navn</option>");
-
-    }
-
-}
-
-function listeboksStudent() {
-
-    include("db-tilkobling.php");
-
-    $sql = "SELECT * FROM student ORDER BY brukernavn;";
-
-    $resultat = mysqli_query($db,$sql) or die("Feil ved henting av studenter: " . mysqli_error($db));
-
-    while ($rad = mysqli_fetch_array($resultat)) {
-
-        $brukernavn = $rad["brukernavn"];
-
-        $fornavn    = $rad["fornavn"];
-
-        $etternavn  = $rad["etternavn"];
-
-        print("<option value='$brukernavn'>$brukernavn - $fornavn $etternavn</option>");
-
-    }
-
+/* Kobling til USN-database */
+ 
+// Bruk informasjonen fra phpMyAdmin
+$host = "b-studentsql-1.usn.no";
+$username = "ahabd4711";
+$password = "212dahabd4711"; // ← skriv inn passordet du bruker i phpMyAdmin
+$database = "ahabd4711";
+ 
+// Forsøk tilkobling
+$db = mysqli_connect($host, $username, $password, $database);
+ 
+// Sjekk om det fungerer
+if (!$db) {
+    die("Feil ved tilkobling til databasen: " . mysqli_connect_error());
 }
  
-// LEGG TIL DENNE NYE FUNKSJONEN:
-
-function listeboksStudentnr() {
-
-    include("db-tilkobling.php");
-
-    $sql = "SELECT * FROM student ORDER BY brukernavn;";
-
-    $resultat = mysqli_query($db,$sql) or die("Feil ved henting av studenter: " . mysqli_error($db));
-
-    while ($rad = mysqli_fetch_array($resultat)) {
-
-        $brukernavn = $rad["brukernavn"];
-
-        $fornavn    = $rad["fornavn"];
-
-        $etternavn  = $rad["etternavn"];
-
-        print("<option value='$brukernavn'>$brukernavn - $fornavn $etternavn</option>");
-
-    }
-
-}
-
+// Tilkoblingen fungerer
+// echo "Tilkobling vellykket!";
 ?>
  
