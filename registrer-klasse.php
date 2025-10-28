f97b1c34c636e9e618aaa7f33fddd365c5141593
Haahm608 <hamseilyas@hotmail.com>
2025-10-28 16:46:24 +0100

    nn

--- registrer-klasse.php
+++ registrer-klasse.php
@@ -2,7 +2,7 @@
 <form method="post" action="">
  
     Klassekode: 
-<input type="text" name="klassekode" maxlength="5" required>
+<input type="text" name="klassekode" maxlength="10" required>
 <br><br>
  
     Klassenavn: 
@@ -19,3 +19,4 @@
  
 <p><a href="vis-alle-klasser.php">Vis alle klasser</a></p>
 <p><a href="index.html">Tilbake til hovedmeny</a></p>
+ 
